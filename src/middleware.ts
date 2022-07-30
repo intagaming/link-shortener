284dac577ed31f901bb975ccb588eb5c437e2fec
An <xuanan2001@pm.me>
2022-07-31 03:00:13 +0700

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname.startsWith("/api/get-url")) {
+  if (
+    req.nextUrl.pathname.startsWith("/api/get-url") ||
+    req.nextUrl.pathname.startsWith("/api/auth")
+  ) {
     return;
   }
 
-  const slug = req.nextUrl.pathname.split("/").pop();
+  const splits = req.nextUrl.pathname.split("/");
+  if (splits.length > 2) return;
+
+  const slug = splits.pop();
   if (slug === "") return;
 
   const data = await (
