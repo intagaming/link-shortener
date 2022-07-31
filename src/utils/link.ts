8bb7fbbcc2219bbf81a6c11898b754837e762d2d
An <xuanan2001@pm.me>
2022-07-31 22:00:38 +0700

    remove https from getBaseUrl public domain

--- src/utils/link.ts
+++ src/utils/link.ts
@@ -1,6 +1,6 @@
 import { env } from "../env/client.mjs";
 
 export const getBaseUrl = () => {
-  if (env.NEXT_PUBLIC_DOMAIN) return `https://${env.NEXT_PUBLIC_DOMAIN}`;
+  if (env.NEXT_PUBLIC_DOMAIN) return env.NEXT_PUBLIC_DOMAIN;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 };
